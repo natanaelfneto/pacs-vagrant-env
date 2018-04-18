272e30db7259f0ec04d6db995b19cc9e08a3dbe3
natanaelfneto <natanaelfneto@outlook.com>
2018-04-18 15:01:22 -0300

    changed home flder for vagrant shared folder

--- jbossbox/shared/start_script.sh
+++ jbossbox/shared/start_script.sh
@@ -2,7 +2,7 @@
 
 # set bashrc file for vagrant image
 echo "setting bashrc file for vagrant image..."
-cp /home/vagrant/shared/script.sh /home/vagrant/.bashrc
+cp /vagrant/shared/script.sh /home/vagrant/.bashrc
 echo "...OK"
 
 # source new bashrc file
