2c088a4bb84b34316108221391bee8072e5d68de
natanaelfneto <natanaelfneto@outlook.com>
2018-04-18 18:30:40 -0300

    update vagrant file to hadle jboss install

--- jbossbox/shared/script.sh
+++ jbossbox/shared/script.sh
@@ -4,10 +4,18 @@
 echo 'begin of vagrant auto script';
 
 # updating and upgrading machine
-apt-get autoremove -y
-apt-get clean
+sudo apt-get autoremove -y
+sudo apt-get clean
 sudo apt-get update;
-# sudo apt-get -y upgrade;
+
+# check if its the first time run of the vagrant image
+echo "check if its the first time run of the vagrant image...";
+if [ ! -f "/vagrant/pacs/firt_time_run" ]; then
+    echo "first time run upgrade..."
+    sudo apt-get -y upgrade;
+    touch "/vagrant/pacs/firt_time_run";
+    echo "...OK"
+fi;
 
 # installing debug requirements
 sudo apt-get install -y git;
@@ -79,23 +87,43 @@ fi;
 
 if [ ! -d "$PACS_DIR" ]; then
     if [ `command -v javac` ]; then
-        # check if dcm4chee is properly located    
+        # check if dcm4chee is properly located
+        echo "check if dcm4chee is properly located..."    
         if [ ! -d "$PACS_DIR/dcm4chee-2.18.1-psql" ]; then
             echo "dcm4chee 2.18.1 PostgreSQL not found, fixing it...";
             unzip $DCM_ZIP -d $PACS_DIR;
+            echo "...OK";
         fi;
-
         # check if jboss is properly located
+        echo "check if jboss is properly located..."
         if [ ! -d "$PACS_DIR/jboss-4.2.3.GA" ]; then
             echo "JBOSS 4.2.3 GA not found, fixing it...";
             unzip $JBOSS_ZIP -d $PACS_DIR;
+            echo "...OK";
+        fi;
+        # check if libclib_jiio.so file is properly located
+        echo "check if libclib_jiio.so file is properly located..."
+        if [ ! -f "$PACS_DIR/jboss-4.2.3.GA/libclib_jiio.so.config" ]; then
+            echo "JBOSS 4.2.3 GA libclib_jiio.so file replacement not found, fixing it...";
+            sudo rm "$PACS_DIR/dcm4chee-2.18.1-psql/bin/native/libclib_jiio.so";
+            sudo cp "$LIB_FILE" "$PACS_DIR/dcm4chee-2.18.1-psql/bin/native/libclib_jiio.so";
+            sudo touch "$PACS_DIR/jboss-4.2.3.GA/libclib_jiio.so.config";
+            echo "...OK";
+        fi;
+        # check if JBOSS is installed
+        echo "check if JBOSS is installed..."
+        if [ ! -f "$PACS_DIR/jboss-4.2.3.GA/jboss_install.config" ]; then
+            echo "JBOSS 4.2.3 GA installation not found, fixing it...";
+            sudo sh "$PACS_DIR/dcm4chee-2.18.1-psql/bin/install_jboss.sh" "$PACS_DIR/jboss-4.2.3.GA"
+            sudo touch "$PACS_DIR/jboss-4.2.3.GA/jboss_install.config";
+            echo "...OK";
         fi;
     else
-        echo "could not find java flag!\n";
-    fi;
+        printf "could not find java!\n";
+    fi; # end of java check
 else
-    echo "could not find pacs dir flag!\n";
-fi;
+    printf "could not find pacs dir!\n";
+fi; # end of pacs dir check
 
 sudo apt-get autoremove -y
 sudo apt-get clean
