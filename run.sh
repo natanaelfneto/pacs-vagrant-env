7d6e14fdd6f6b326a56148b01394a83d1b6f5187
natanaelfneto <natanaelfneto@outlook.com>
2018-04-19 11:44:00 -0300

    run.sh update for restart bugfix

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 (cd ./jbossbox; vagrant halt --force;);
 vboxmanage hostonlyif remove vboxnet1
 vboxmanage hostonlyif remove vboxnet0
-vboxmanage hostonlyif create
-sudo ip link set vboxnet0 up
-ifconfig vboxnet0 192.168.68.8 netmask 255.255.255.0 up
+boxmanage hostonlyif create
+vboxmanage hostonlyif ipconfig --ip 192.168.68.8 vboxnet0
 vagrant global-status
 (cd ./jbossbox/; vagrant up --provision;);
